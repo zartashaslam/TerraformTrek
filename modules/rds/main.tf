97626a7d33660b5e796e4b5f4741a179352c0219
zartash aslam <macbookpro@Macbooks-MacBook-Pro.local>
2024-03-14 00:31:26 +0500

    few changes

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_name              = "mydb"
   engine               = "mysql"
-  engine_version       = "5.7"
+  engine_version       = "5.8"
   instance_class       = "db.t3.micro"
   username             = "foo"
   password             = "foobarbaz"
